a5cac9171bba97ba0eae4b77fc4341535374ba79
Eric Chamberlain <eric.chamberlain@hotmail.com>
2018-11-26 12:55:45 -0800

    Initial project

---
+++ GraphQLPagination/GraphQLTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GraphQLPagination
+//
+//  Created by Eric Chamberlain on 11/26/18.
+//  Copyright © 2018 Eric Chamberlain. All rights reserved.
+//
+
+import UIKit
+
+class GraphQLTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
